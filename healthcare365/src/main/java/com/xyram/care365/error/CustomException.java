c3d162236c1c02f0fd92c1706c24ee0c769d1566
Raju <raju.ghosh527@gmail.com>
2020-12-14 12:31:01 +0530

    new call is added

---
+++ healthcare365/src/main/java/com/xyram/care365/error/CustomException.java
@@ -0,0 +1,13 @@
+package com.xyram.care365.error;
+
+public class CustomException extends Exception{
+	
+	public CustomException() {
+		super();
+	}
+
+	public CustomException(String error) {
+		super(error);
+	}
+
+}
